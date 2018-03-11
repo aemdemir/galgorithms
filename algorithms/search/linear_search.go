8aae1449a4bfb823d2ee2a48ca358194704b656e
emredemir <aemdemir@gmail.com>
2018-03-12 01:17:59 +0300

    Linear Search implementation

---
+++ algorithms/search/linear_search.go
@@ -0,0 +1,11 @@
+package search
+
+// LinearSearch searches for target element in given array.
+func LinearSearch(arr []int, target int) int {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == target {
+			return i
+		}
+	}
+	return -1
+}
