d0cef93ee62bbe5ce530a50b6582ae3bbce76e27
emredemir <aemdemir@gmail.com>
2018-03-28 21:07:00 +0300

    Depth First Search implementation

---
+++ data-structures/graph/dfs.go
@@ -0,0 +1,48 @@
+package graph
+
+import (
+	"github.com/emre-demir/go-algorithms/data-structures/stack"
+)
+
+// DepthFirstSearch explores a branch as far as possible until it
+// reaches the end of the branch. At that point, it backtracks,
+// and explores the next available branch.
+func DepthFirstSearch(g *Graph, source, destination Vertex) []Vertex {
+	s := stack.New()
+	s.Push(source)
+
+	visited := make(map[Vertex]bool)
+	visited[source] = true
+
+OUTER:
+	for vertex := s.Peek(); vertex != nil && vertex != destination; vertex = s.Peek() {
+		u := vertex.(Vertex)
+
+		adjacents := g.Adj(u)
+		if !(len(adjacents) > 0) {
+			s.Pop()
+			continue
+		}
+		for _, v := range adjacents {
+			if _, ok := visited[v]; !ok {
+				s.Push(v)
+				visited[v] = true
+				continue OUTER
+			}
+		}
+
+		s.Pop()
+	}
+
+	if s.IsEmpty() {
+		return nil
+	}
+
+	slice := s.ToSlice()
+	vertices := make([]Vertex, len(slice))
+	for i, e := range slice {
+		vertices[i] = e.(Vertex)
+	}
+
+	return vertices
+}
